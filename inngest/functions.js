99f7e26748afdd2b308999d90516ad7da6912e83
casleen <casleen.global@gmail.com>
2025-11-09 21:36:05 +0800

    mobfiy

---
+++ inngest/functions.js
@@ -0,0 +1,47 @@
+import prisma from "@/lib/prisma";
+import { inngest } from "./client";
+
+export const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-create" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+      },
+    });
+  }
+);
+
+//Inngest Function to update user data in database(导入函数用于更新数据库中的用户数据)
+export const syncUserUpdation = inngest.createFunction(
+  { id: "sync-user-update" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.update({
+      where: { id: data.id },
+      data: {
+        id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+      },
+    });
+  }
+);
+
+export const syncUserDeletion = inngest.createFunction(
+  { id: "sync-user-delete" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.delete({
+      where: { id: data.id },
+    });
+  }
+);
